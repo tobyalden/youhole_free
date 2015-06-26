b33235bf4f256ab9a812433727a14d1804910416
Toby Alden <toby.alden@gmail.com>
2015-06-25 22:00:42 -0700

    YouHole Beta

--- js/scripts.js
+++ js/scripts.js
@@ -33,12 +33,70 @@ function onPlayerReady(event) {
 //    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.ENDED && queue.length > 0) {
-    nextVideo = queue.shift();
-    console.log("video finished! nextVideo = " + nextVideo.title);
-    player.loadVideoById(nextVideo.id);
+  if (event.data == YT.PlayerState.ENDED) {
+    if(queue.length > 0) {
+      nextVideo = queue.shift();
+      console.log("video finished! nextVideo = " + nextVideo.title);
+      player.loadVideoById(nextVideo.id);
+    } else {
+      randomWord();
+    }
     drawPage();
   }
+}
+
+var viewCountThreshold = 500;
+var keywordBlacklist = ["pronounce", "say", "vocabulary", "spelling", "mean", "definition", "slideshow"]
+
+function randomWord() {
+  var requestStr = "http://randomword.setgetgo.com/get.php";
+  // $.getJSON(requestStr).then(function(responseJSON0) {
+  //   randomVideo(responseJSON0);
+  // });
+
+  $.ajax({
+      type: "GET",
+      url: requestStr,
+      dataType: "jsonp",
+      jsonpCallback: 'randomVideo'
+  });
+}
+
+function randomVideo(data) {
+  console.log(data.Word);
+  var url = "https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&q=" + data.Word + "&type=video&maxResults=50&key=AIzaSyAHu80T94GGhKOzjBs9z5yr0KU8v48Zh60";
+  $.getJSON(url).then(function(responseJSON) {
+    if (responseJSON.items.length < 1) {
+      console.log("No videos found for " + data.Word + "Restarting search.");
+      randomWord();
+    } else {
+        var videoId = responseJSON.items[0].id.videoId;
+        var url2 = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id=" + videoId + "&key=AIzaSyAHu80T94GGhKOzjBs9z5yr0KU8v48Zh60";
+        $.getJSON(url2).then(function(responseJSON2) {
+          if(responseJSON2.items[0].statistics.viewCount > viewCountThreshold) {
+            console.log("View count too high. Restarting search.");
+            randomWord();
+          } else if(isBlacklisted(responseJSON2.items[0].snippet.title, responseJSON2.items[0].snippet.description)) {
+            console.log("Title: " + responseJSON2.items[0].snippet.title + " - Description: " + responseJSON2.items[0].snippet.description + " contains blacklisted word. Restarting search.")
+            randomWord();
+          } else {
+            console.log("Success! Video ID = " + responseJSON2.items[0].id);
+            player.loadVideoById(responseJSON2.items[0].id);
+          }
+        });
+      }
+  });
+}
+
+function isBlacklisted(title, description) {
+  title = title.toLowerCase();
+  description = description.toLowerCase();
+  for(var i = 0; i < keywordBlacklist.length; i++) {
+    if(title.includes(keywordBlacklist[i]) || description.includes(keywordBlacklist[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // jQuery
@@ -77,6 +135,12 @@ $(document).ready(function() {
     });
 
     $("input#videoUrl").val("");
+  });
+
+  $("#randomVideo").click(function(event) {
+    event.preventDefault();
+    console.log("randomVideo clicked");
+    randomWord();
   });
 
   drawPage = function drawPage() {
